262b6dc257f89a5ecd8bb3e3d3403d43dfe2c582
Sennay <wsennay@gmail.com>
2022-08-09 13:36:40 +0300

    fix:add find statuses count function

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -36,7 +36,8 @@ class TweetDfExtractor:
 
     # an example function
     def find_statuses_count(self)->list:
-        statuses_count 
+        statuses_count = [x['user']['statuses_count'] for x in self.tweets_list]
+        return statuses_count
         
     def find_full_text(self)->list:
         text = [x['full_text'] for x in tweet_list]
