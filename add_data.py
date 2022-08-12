270e7bc149651c804be8d172074b2fd0090b8c73
Sennay <wsennay@gmail.com>
2022-08-12 21:17:28 +0300

    add insert tweets to db function and modify unwanted columns

--- add_data.py
+++ add_data.py
@@ -52,7 +52,7 @@ def pre_process(df: pd.DataFrame) -> pd.DataFrame:
     df = cl.remove_non_english_tweets(df)
     df = cl.convert_to_numbers(df)
 
-    drop = ['possibly_sensitive', 'original_text']
+    drop = ['possibly_sensitive', 'original_text', 'user_mentions', 'hashtags']
     try:
         df = df.drop(drop, axis=1)
         df = df.fillna(0)
@@ -62,4 +62,27 @@ def pre_process(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
+def insert_tweet_to_table():
+    conn, cur = db_connect()
+    df = get_df()
+    df = pre_process(df)
+    for _, row in df.iterrows():
+        sqlQuery = f"""INSERT INTO TweetInformation (created_at, source, polarity, subjectivity, lang,
+                        favorite_count, retweet_count, screen_name, original_author, followers_count, friends_count
+                        , place, clean_text)
+                 VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"""
+        data = (row[0], row[1], row[2], row[3], (row[4]), (row[5]), row[6], row[7], row[8], row[9], row[10], row[11],
+                row[12])
 
+        try:
+            # Execute the SQL command
+            cur.execute(sqlQuery, data)
+            # Commit your changes in the database
+            conn.commit()
+        except Error as e:
+            conn.rollback()
+            print("Error: ", e.args)
+    print('data inserted')
+
+
+insert_tweet_to_table()
